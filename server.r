4974242b7d14fa26fd3acb735551baa8d7bd1ee8
David A McAllister <david.mcallister@glasgow.ac.uk>
2018-05-14 19:50:25 +0100

    Working version of app Working version of app. Use native ggplot2 rather than plotly.

--- server.r
+++ server.r
@@ -1,1 +1,44 @@
 # server.r
+
+library(shiny)
+library(ggplot2)
+library(tidyverse)
+
+
+load(file = "Data/HFbasic_for_shiny_app.Rdata")
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  # Round age to accomodate not modelled values
+     age_rounder <- reactive({
+       if(input$age %in% c(43L, 48L, 53L, 58L, 61L, 81L, 86L)) input$age + 1L else input$age
+     })
+     
+  # Select data based on age, deprivation and whether absolute or relative scale
+     hf_smry_time <- reactive({
+    if(input$myscale == "Absolute") {
+       mydf <- data_for_plot_time_mod_time_s_absolute } else mydf <-  data_for_plot_time_mod_time_s_relative
+    mydf %>% 
+     filter(dep_two == input$deprivation & age_ten == age_rounder())
+   })
+   
+     # Create plot
+   output$TimePlot <- renderPlot({
+      plot_time <- ggplot(hf_smry_time(),
+                        aes(x = year, y = est, colour = type, group = interaction(type))) + 
+      geom_line() +
+      geom_ribbon(alpha = 0.1, mapping = aes(ymin = lower, ymax = upper, fill = type)) +
+      facet_grid (~sex)  +
+      scale_color_discrete("") + 
+      scale_fill_discrete(("")) +
+      scale_y_continuous("Rate per 1000 person-years", breaks = log(2^(-3:6)), labels = 2^(-3:6)) +
+      scale_x_continuous("Calendar years") +
+      scale_size(guide = FALSE) +
+      scale_linetype(guide = FALSE)
+    if (input$myscale == "Absolute") plot_time <- plot_time + 
+      scale_y_continuous("Rate per 1000 person-years")
+    
+    
+    plot_time
+   })
+}
--- ui.r
+++ ui.r
@@ -1,1 +1,40 @@
-ui.r
+# ui.r
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+   
+   # Application title
+   titlePanel("Figure 3"),
+   
+   # Sidebar with a slider input for number of bins 
+   sidebarLayout(
+      sidebarPanel(
+         sliderInput("age",
+                     "Age (years):",
+                     min = 20,
+                     max = 90,
+                     value = 30,
+                     round = TRUE),
+         sliderInput("deprivation",
+                     "Deprivation (deciles, most to least):",
+                     min = 1,
+                     max = 10,
+                     value = TRUE),
+         selectInput("myscale", "Choose a scale:",
+         choices = c("Absolute", "Relative"),
+         selected = "Absolute")
+      ),
+      
+      
+      # Show a plot of the generated distribution
+      mainPanel(
+         plotOutput("TimePlot"),
+         h1("Instructions"),
+         p("This interactive plot corresponds to Figure 3 in the manuscript.
+           We will add an informative heading as well as text explaining figure when the findings have been published.
+           At present this information has been withheld to preserve the novelty of the publication.
+           Choose whether to plot the rate for different age and deprivation levels, and whether to view the rates
+           on the relative or log-scale.")
+      )
+   )
+)
